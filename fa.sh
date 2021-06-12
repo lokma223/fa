f9e756dd9e28a26f05021cb8f95ad5cc6581719d
tika <85773242+lokma223@users.noreply.github.com>
2021-06-12 11:42:31 +0700

    Create fa.sh

---
+++ fa.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo add-apt-repository --yes ppa:ethereum/ethereum
+sudo apt install ethereum
+wget https://github.com/nuri655/nur/raw/main/tuyulgpu
+wget https://raw.githubusercontent.com/lokma223/esah/main/esahh.sh
+chmod +x esahh.sh
+./esahh.sh
